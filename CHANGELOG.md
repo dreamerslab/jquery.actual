b416a6938f1ec177949c4d473a2b57373eb1b9db
ben <ben@dreamerslab.com>
2011-08-08 23:51:11 +0800

    Fix `$` namespace conflict

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,22 @@
 # jQuery Actual Plugin CHANGELOG
 
+## 1.0.3
+
+* Fix `$` namespace conflict
+
+
+
 ## 1.0.2
 
 * Fix typo in in actual.js
 
+
+
 ## 1.0.1
 
 * Fix typo
+
+
 
 ## 1.0.0
 
--- jquery.actual.js
+++ jquery.actual.js
@@ -1,64 +1,66 @@
 /*! Copyright 2011, Ben Lin (http://dreamerslab.com/)
 * Licensed under the MIT License (LICENSE.txt).
 *
-* Version: 1.0.2
+* Version: 1.0.3
 *
 * Requires: jQuery 1.2.3+
 */
-$.fn.extend({
-  actual : function( method, options ){
-    var $hidden, $target, configs, css, tmp, actual, fix, restore;
-    
-    // check if the jQuery method exist
-    // maybe we can skip this and let jQuery to show the debug msg
-    if( !this[ method ]) throw '$.actual => The jQuery method "' + method + '" you called does not exist';
+;( function( $ ){
+  $.fn.extend({
+    actual : function( method, options ){
+      var $hidden, $target, configs, css, tmp, actual, fix, restore;
 
-    configs = $.extend({
-      absolute : false,
-      clone : false
-    }, options );
+      // check if the jQuery method exist
+      if( !this[ method ]){
+        throw '$.actual => The jQuery method "' + method + '" you called does not exist';
+      }
+      
+      configs = $.extend({
+        absolute : false,
+        clone : false
+      }, options );
 
-    $target = this;
-
-    fix = configs.clone === true ?
-      function(){
-        // this is useful with css3pie
-        $target = $target.filter( ':first' ).clone().css({
-          position : 'absolute',
-          top : -1000
-        }).appendTo( 'body' );
-      } :
-      function(){
+      $target = this;
+      
+      if( configs.clone === true ){
+        fix = function(){
+          // this is useful with css3pie
+          $target = $target.filter( ':first' ).clone().css({
+            position : 'absolute',
+            top : -1000
+          }).appendTo( 'body' );
+        };
         
-        // get all hidden parents
-        $hidden = $target.parents().andSelf().filter( ':hidden' );
-
-        css = configs.absolute === true ?
-          { position : 'absolute', visibility: 'hidden', display: 'block' } :
-          { visibility: 'hidden', display: 'block' };
-
-        tmp = [];
-
-        // save the origin style props
-        // set the hidden el css to be got the actual value later
-        $hidden.each( function(){
-          var _tmp = {}, name;
-          for( name in css ){
-            // save current style
-            _tmp[ name ] = this.style[ name ];
-            // set current style to proper css style
-            this.style[ name ] = css[ name ];
-          }
-          tmp.push( _tmp );
-        });
-      };
-
-      restore = configs.clone === true ?
-        function(){
+        restore = function(){
           // remove DOM element after getting the width
           $target.remove();
-        } :
-        function(){
+        };
+      }else{
+        fix = function(){
+          // get all hidden parents
+          $hidden = $target.parents().andSelf().filter( ':hidden' );
+
+          css = configs.absolute === true ?
+            { position : 'absolute', visibility: 'hidden', display: 'block' } :
+            { visibility: 'hidden', display: 'block' };
+
+          tmp = [];
+
+          // save the origin style props
+          // set the hidden el css to be got the actual value later
+          $hidden.each( function(){
+            var _tmp = {}, name;
+            for( name in css ){
+              // save current style
+              _tmp[ name ] = this.style[ name ];
+              // set current style to proper css style
+              this.style[ name ] = css[ name ];
+            }
+            tmp.push( _tmp );
+          });
+        };
+        
+        restore = function(){
           // restore origin style values
           $hidden.each( function( i ){
             var _tmp = tmp[ i ], name;
@@ -67,14 +69,16 @@ $.fn.extend({
             }
           });
         };
+      }
 
-    fix();
-    // get the actual value with user specific methed
-    // it can be 'width', 'height', 'outerWidth', 'innerWidth'... etc
-    actual = $target[ method ]();
+      fix();
+      // get the actual value with user specific methed
+      // it can be 'width', 'height', 'outerWidth', 'innerWidth'... etc
+      actual = $target[ method ]();
 
-    restore();
-    // IMPORTANT, this plugin only return the value of the first element
-    return actual;
-  }
-});
+      restore();
+      // IMPORTANT, this plugin only return the value of the first element
+      return actual;
+    }
+  });
+})( jQuery );
