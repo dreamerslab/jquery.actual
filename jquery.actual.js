d97125853c058cfea70f880f37fdc2ca77874ae2
Jacob <jacobq@gmail.com>
2013-07-19 10:13:01 -0500

    Version bump

--- jquery.actual.js
+++ jquery.actual.js
@@ -1,9 +1,9 @@
 /*! Copyright 2012, Ben Lin (http://dreamerslab.com/)
  * Licensed under the MIT License (LICENSE.txt).
  *
- * Version: 1.0.14
+ * Version: 1.0.15
  *
- * Requires: jQuery 1.2.3 ~ 1.9.0
+ * Requires: jQuery >= 1.2.3
  */
 ;( function ( $ ){
   $.fn.extend({
